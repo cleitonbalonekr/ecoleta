d6ba8b5e0f7a5e6cd17e8ca9eb64d938e80264b6
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-06-05 15:22:19 -0300

    App

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,19 @@
+import knex from "../database/connection";
+import { Request, Response } from "express";
+class ItemsController {
+  public async index(request: Request, response: Response) {
+    const items = await knex("items").select("*");
+
+    const serializadeItems = items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://192.168.1.18:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializadeItems);
+  }
+}
+
+export default new ItemsController();
